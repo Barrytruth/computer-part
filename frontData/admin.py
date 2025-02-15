83314da7de7e3b0b880b57193833ed5f9714d6fa
Barrytruth <barrytruth@hotmail.com>
2025-02-15 08:10:57 +0800

    admin register models to see db.sqlite3

--- frontData/admin.py
+++ frontData/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from dataCollect.models import autoupdateLog, partList
 # Register your models here.
+
+admin.site.register(autoupdateLog)
+admin.site.register(partList)
