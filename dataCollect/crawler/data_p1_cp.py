c14c30647520c77a3127ca9cbbe3c36c7f734ac1
Barrytruth <barrytruth@hotmail.com>
2025-02-16 08:35:16 +0800

    adjest width

--- dataCollect/crawler/data_p1_cp.py
+++ dataCollect/crawler/data_p1_cp.py
@@ -16,7 +16,7 @@ def setup_driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_service = Service(os.environ.get("CHROMEDRIVER_PATH"))
     driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
     return driver
